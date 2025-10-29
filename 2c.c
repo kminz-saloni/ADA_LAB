72d83d541ad31238708757da5b4914f5289bb064
KARAN MINJ <karanminj9717@gmail.com>
2025-10-29 22:20:48 +0530

    Replace custom insertion sort with standard implementation

--- 2c.c
+++ 2c.c
@@ -2,39 +2,52 @@
 #include <stdlib.h>
 #include <time.h>
 
-// Modified insertion sort function
-void sortByInsertion(int dataset[], int length) {
-    for (int pos = 1; pos < length; pos++) {
-        int temp = dataset[pos];
-        int index = pos - 1;
-        while (index >= 0 && dataset[index] > temp) {
-            dataset[index + 1] = dataset[index];
-            index--;
+// Insertion Sort implementation
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
         }
-        dataset[index + 1] = temp;
+        arr[j + 1] = key;
     }
 }
 
-int main() {
-    int test_sizes[] = {100, 300, 600, 900, 1200, 1500, 1800};
-    int test_count = sizeof(test_sizes) / sizeof(test_sizes[0]);
-    srand(time(NULL));
+// Optional: print array contents
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
 
-    for (int k = 0; k < test_count; k++) {
-        int length = test_sizes[k];
-        int *dataset = (int*) malloc(length * sizeof(int));
-        // Initialize random data
-        for (int m = 0; m < length; m++) {
-            dataset[m] = rand();
+int main() {
+    int sizes[] = {10, 1000, 10000, 100000, 1000000};
+    srand((unsigned int)time(NULL));
+
+    for (int s = 0; s < 5; s++) {
+        int n = sizes[s];
+        int *arr = (int*)malloc(n * sizeof(int));
+        if (!arr) {
+            printf("Memory allocation failed for size %d\n", n);
+            continue;
         }
 
-        clock_t begin = clock();
-        sortByInsertion(dataset, length);
-        clock_t finish = clock();
+        for (int i = 0; i < n; i++)
+            arr[i] = rand() % 10000;
 
-        double duration = (double)(finish - begin) / CLOCKS_PER_SEC;
-        printf("Array size: %d, Duration: %lf seconds\n", length, duration);
-        free(dataset);
+        printf("Test case %d: Size = %d\n", s + 1, n);
+        clock_t start = clock();
+        insertionSort(arr, n);
+        clock_t end = clock();
+
+        double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+        printf("Time taken: %f seconds\n\n", time_taken);
+
+        free(arr);
     }
+
     return 0;
 }
